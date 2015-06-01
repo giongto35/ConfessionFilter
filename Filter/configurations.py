5050185c4f29ef38366fefd7bce59c10efa57553
Tmbao <tmbao@apcs.vn>
2015-06-01 13:13:59 +0700

    Update project structure

--- Filter/configurations.py
+++ Filter/configurations.py
@@ -1,8 +1,13 @@
+import os
+
+# Base directory
+__base_dir__ = os.path.dirname(__file__)
+
 # Training directory
-training_directory = 'training_data'
+training_directory = os.path.join(__base_dir__, '../Data', 'training')
 
 # Testing directory
-testing_directory = 'testing_data'
+testing_directory = os.path.join(__base_dir__, '../Data', 'testing')
 
 # Raw documents
 raw_document_folder = 'raw'
